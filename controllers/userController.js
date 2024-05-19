6e9e5bfe4f68e7517e3ab3abf0fb9ef647dc7ec4
Charles Mc Vigo <cvgithub204@gmail.com>
2024-05-19 11:47:02 +0800

    user routes done

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,3 +68,16 @@ exports.updateUser = async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 };
+
+// Delete a user
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        await User.deleteOne({ _id: req.params.id });
+        res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
